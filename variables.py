35d94fe9d5853bfab4874e1c2f556a8454475f4b
Haziz Khan <hazizkhan1999@gmail.com>
2017-06-12 13:53:54 +0500

    variables done

---
+++ variables.py
@@ -0,0 +1,10 @@
+DOB = 2000
+age = 2017 - DOB
+name = "Haziz"
+ageMessage = "hey! your age is {}".format(age)
+greetingsWithAge = "Hey! {}, your age is {}".format(name, age);
+l = upper(greetingsWithAge)
+print(DOB)
+print(age)
+print(ageMessage)
+print(greetingsWithAge)
